ec44549c76a48cbebdc0cebb0559f9939f29707a
frexjhon <1063192208@qq.com>
2019-02-15 18:54:58 +0800

    新闻列表相关界面

---
+++ app/component/NewsItemPage.js
@@ -0,0 +1,209 @@
+import React, {Component} from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    FlatList,
+    Image,
+    TouchableOpacity,
+    ToastAndroid,
+    Dimensions
+} from 'react-native';
+
+const {width, height} = Dimensions.get('window');
+export default class NewsItemPage extends Component {
+
+    static defaultProps = {
+        url: 'http://v.juhe.cn/toutiao/index?key=d78b502268f7456b79fbe7228cecdd46&type='
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                // {
+                //     thumbnail_pic_s: "http://06.imgmini.eastday.com/mobile/20180621/20180621185141_f561ba39d77a3362e95ae7c816dce18e_1_mwpm_03200403.jpg",
+                //     title: 'tittitletitletitletitletitletitletitltittitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitleleetitletitletitletitletitletitletitletitletitlele',
+                //     date: "2018-06-21 18:51",
+                //     author_name: '金十数据',
+                //     uniquekey: 'da',
+                //     url:'http://xiaweizi.cn'
+                // }
+            ], //存储列表使用的数据
+            refreshing: false, //当前的刷新状态
+            loadingVisible: true,
+            type: props.type,
+            navigation: props.navigation,
+            netErrorVisible: false,
+        };
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <FlatList
+                    showsVerticalScrollIndicator={false}
+                    data={this.state.data}
+                    keyExtractor={this.keyExtractor}
+                    renderItem={({item}) => {
+                        return this.getView(item, this.state.navigation)
+                    }}
+
+                    //下拉刷新，必须设置refreshing状态
+                    onRefresh={this.onRefresh}
+                    refreshing={this.state.refreshing}
+                />
+                {/*{*/}
+                    {/*this.state.loadingVisible ? (*/}
+                        {/*<Loading/>*/}
+                    {/*) : null*/}
+                {/*}*/}
+                {
+                    // this.state.netErrorVisible ? (
+                    //     <NetworkFailureLayout retryClick={()=>{
+                    //         this.setState({
+                    //             loadingVisible: true,
+                    //             netErrorVisible: false,
+                    //         });
+                    //         this.getData()
+                    //     }} />
+                    // ) : null
+                }
+            </View>
+        );
+    }
+
+    getView(item, navigation) {
+        //这里返回的就是每个Item
+        return (
+            <TouchableOpacity activeOpacity={0.8}
+                              onPress={() => {
+                                  }}>
+                <View style={styles.item}>
+                    <View style={styles.news_content}>
+                        <Text style={styles.new_title} numberOfLines={3}>
+                            {item.title}
+                        </Text>
+                        <Text style={styles.new_bottom} numberOfLines={1}>
+                            {item.author_name} {item.date}
+                        </Text>
+                    </View>
+
+                    <View>
+                        <Image source={{uri: item.thumbnail_pic_s}} style={styles.image}/>
+                    </View>
+
+                </View>
+            </TouchableOpacity>
+        )
+    };
+
+    keyExtractor = (item, index) => item.uniquekey;
+
+    /**
+     * 下拉属性
+     */
+    onRefresh = () => {
+        //设置刷新状态为正在刷新
+        this.setState({
+            refreshing: true,
+            loadingVisible: true,
+        });
+        this.getData();
+    };
+
+    //渲染完成，请求网络数据
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData() {
+        fetch(this.props.url + this.state.type)
+            .then((response) => response.json())
+            .then((response) => {
+                //解析json数据
+                this.onSuccess(response)
+            })
+            .catch((error) => {
+                //网络错误处理
+                console.log('error', error);
+                this.onFailed(String.public_net_error)
+            });
+    }
+
+    onSuccess(response) {
+        console.log('NewsItem', response);
+        if (response.error_code == 0) {
+            this.setState({
+                data: response.result.data,
+                loadingVisible: false,
+                refreshing: false,
+                netErrorVisible: false,
+            });
+        } else {
+            this.onFailed(response.reason)
+        }
+    }
+
+    onFailed(msg) {
+        console.log('NewsItem', msg);
+        this.setState({
+            loadingVisible: false,
+            refreshing: false,
+            netErrorVisible: true,
+        });
+        ToastAndroid.show(msg, ToastAndroid.SHORT);
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    item: {
+        width: width - 10,
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderWidth: 0,
+        borderRadius: 5,
+        borderColor: 'rgba(0,0,0,0.1)',
+        backgroundColor: '#fff',
+        margin: 5,
+        height: 120,
+        padding: 15,
+        shadowColor: '#ccc',
+        shadowOffset: {width: 2, height: 2,},
+        shadowOpacity: 0.5,
+        shadowRadius: 10,
+        elevation: 3,
+        overflow: 'hidden',
+    },
+    image: {
+        width: 120,
+        height: 100,
+
+        borderRadius: 5,
+        marginRight: 5,
+    },
+    news_content: {
+        width: width - 120,
+        height: 110,
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+    },
+    new_title: {
+        marginRight: 5,
+        overflow: 'hidden',
+        color: '#363636',
+        fontSize: 22,
+    },
+    new_bottom: {
+        color: '#363636',
+        fontSize: 22,
+    }
+});
--- app/component/NewsPage.js
+++ app/component/NewsPage.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {Text} from "react-native";
 import {Actions} from "react-native-router-flux";
+import ScrollableTabView, {ScrollableTabBar} from "react-native-scrollable-tab-view-fix-guo";
+import NewsItemPage from "./NewsItemPage";
 
 
 export default class NewsPage extends Component {
@@ -13,9 +15,38 @@ export default class NewsPage extends Component {
 
     render() {
         return (
-            <Text onPress={() => Actions.today()}>
-                新闻
-            </Text>
+            <ScrollableTabView
+                //overlayTop 悬浮在内容视图之上 overlayBottom bottom
+                tabBarPosition='top'
+                renderTabBar={() => <ScrollableTabBar/>}
+                onChangeTab={(obj) => {
+                    //i：被选中的Tab的下标（从0开始）
+                    // ref：被选中的Tab对象（基本用不到）
+
+                }}
+                onScroll={(postion) => {
+                    // float类型 [0, tab数量-1]
+                    console.log('scroll position:' + postion);
+                }}
+                locked={false}//是否可拖动 默认false
+                initialPage={1}//初始页面角标
+                //tabBarUnderlineStyle(style) 下划线设置
+                //tabBarBackgroundColor(String)
+                //tabBarActiveTextColor(String)
+                //tabBarInactiveTextColor(String)
+                //tabBarTextStyle(Object)
+                // scrollWithoutAnimation={true}//(Bool，默认为false)
+            >
+                <NewsItemPage tabLabel='头条' type='top'/>
+                <NewsItemPage tabLabel='社会' type='shehui'/>
+                <NewsItemPage tabLabel='国内' type='guonei'/>
+                <NewsItemPage tabLabel='国际' type='国际'/>
+                <NewsItemPage tabLabel='娱乐' type='yule'/>
+                <NewsItemPage tabLabel='体育' type='tiyu'/>
+                <NewsItemPage tabLabel='军事' type='junshi'/>
+                <NewsItemPage tabLabel='科技' type='keji'/>
+                <NewsItemPage tabLabel='财经' type='caijing'/>
+            </ScrollableTabView>
         );
     }
 
